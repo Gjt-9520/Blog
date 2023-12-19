af151e42070f5560198916bf9daf422ee66cc7d8
Gjt-9520 <1538048299@qq.com>
2023-12-19 17:29:30 +0800

    test everything

--- src/site.config.ts
+++ src/site.config.ts
@@ -1,19 +1,13 @@
 import type { SiteConfig } from "@/types";
 
 export const siteConfig: SiteConfig = {
-	// Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
-	author: "Chris Williams",
-	// Meta property used to construct the meta title property, found in src/components/BaseHead.astro L:11
-	title: "Astro Theme Cactus",
-	// Meta property used as the default description meta property
-	description: "An opinionated starter theme for Astro",
-	// HTML lang property, found in src/layouts/Base.astro L:18
-	lang: "en-GB",
-	// Meta property, found in src/components/BaseHead.astro L:42
-	ogLocale: "en_GB",
-	// Date.prototype.toLocaleDateString() parameters, found in src/utils/date.ts.
+	author: "Gujintao",
+	title: "今朝博客",
+	description: "",
+	lang: "zh-CN",
+	ogLocale: "zh-CN",
 	date: {
-		locale: "en-GB",
+		locale: "zh-CN",
 		options: {
 			day: "numeric",
 			month: "short",
@@ -25,18 +19,14 @@ export const siteConfig: SiteConfig = {
 	},
 };
 
-// Used to generate links in both the Header & Footer.
 export const menuLinks: Array<{ title: string; path: string }> = [
+
 	{
-		title: "Home",
+		title: "最新提交",
 		path: "/",
 	},
 	{
-		title: "About",
-		path: "/about/",
-	},
-	{
-		title: "Blog",
+		title: "全部文章",
 		path: "/posts/",
 	},
 ];
